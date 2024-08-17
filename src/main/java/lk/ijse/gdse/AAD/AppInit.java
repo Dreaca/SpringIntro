62e5699ca68ffd16c9a38b98b755b2d8f2a52e0f
dreaca <buddhikadreaca@gmail.com>
2024-08-17 15:38:09 +0530

    Comments

--- src/main/java/lk/ijse/gdse/AAD/AppInit.java
+++ src/main/java/lk/ijse/gdse/AAD/AppInit.java
@@ -8,7 +8,9 @@ public class AppInit {
         var ctx = new AnnotationConfigApplicationContext(); // Creates a spring annotation context or a bucket that can hold spring objects known as beans
         ctx.register(Config.class); //The agent that detects components classes
         ctx.refresh(); // Refresh the class to find the necessary classes
-        ctx.close(); // Close the class
+        // ctx.close();  Close the class
+        ctx.registerShutdownHook(); // gracefully shutdown the application
+
 
     }
 }
